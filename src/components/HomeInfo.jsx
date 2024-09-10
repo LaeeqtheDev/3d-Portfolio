5af1cf949a2ebcaf1e89fa505b94810d5eb893a5
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-09-10 14:28:32 -0500

    Update HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -10,7 +10,7 @@ const HomeInfo = ({ currentStage }) => {
         <span className='font-semibold mx-2 text-white'>Laeeq</span>
         👋
         <br />
-        A FullStack Developer with Expertise in React/Next.js & React-Native from Lahore, Pk
+        A FullStack Developer with Expertise in React/Next Js & ReactNative from Lahore, Pk
       </h1>
     );
 
