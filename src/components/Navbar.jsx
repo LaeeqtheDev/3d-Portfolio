0eab54093766b50759b4bef51bdb20115a4948bb
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2025-05-15 12:59:55 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,32 @@
-import { NavLink } from "react-router-dom"
-
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className="header">
-    <NavLink to="/" className="p-2 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-    <p className="blue-gradient_text">LaeeqtheDev</p>
-    </NavLink>
-    <nav className="flex text-lg gap-7 font-medium">
-    <NavLink to="/about" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
-    About
-    </NavLink>
+      <NavLink to="/" className="p-2 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <p className="blue-gradient_text">LaeeqtheDev</p>
+      </NavLink>
 
-    <NavLink to="/projects" className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
-    Projects
-    </NavLink>
-    
-    </nav>
-    
-    
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+          About
+        </NavLink>
+
+        <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+          Projects
+        </NavLink>
+
+        <a
+          href="https://linktr.ee/syedlaeeqahmed"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black hover:text-blue-500"
+        >
+          Links
+        </a>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
