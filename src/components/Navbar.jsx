a7b6805f50174403b602c385a2f64717e1030362
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2025-11-09 02:24:16 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,26 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="header">
-      <NavLink to="/" className="p-2 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+    <header className="header flex flex-col md:flex-row items-center justify-between p-4 bg-white shadow-md">
+      <NavLink
+        to="/"
+        className="p-2 rounded-lg bg-white flex items-center justify-center font-bold shadow-md mb-3 md:mb-0"
+      >
         <p className="blue-gradient_text">LaeeqtheDev</p>
       </NavLink>
 
-      <nav className="flex text-lg gap-7 font-medium">
-        <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+      <nav className="flex flex-col md:flex-row text-lg gap-4 md:gap-7 font-medium items-center">
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "text-blue-500" : "text-black")}
+        >
           About
         </NavLink>
 
-        <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) => (isActive ? "text-blue-500" : "text-black")}
+        >
           Projects
         </NavLink>
 
